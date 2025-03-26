2b4fa6a71e08088c81c05dc70fdafd87af373108
Dimi-Sama <dimitry97490@gmail.com>
2025-03-26 14:01:38 +0400

    fix ttest

--- src/test/java/com/shiki/echo_waves/EchoWavesApplicationTests.java
+++ src/test/java/com/shiki/echo_waves/EchoWavesApplicationTests.java
@@ -2,8 +2,10 @@ package com.shiki.echo_waves;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EchoWavesApplicationTests {
 
 	@Test
